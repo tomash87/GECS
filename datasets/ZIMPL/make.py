db489481a380e1235ea4b22916fe89b483bca94d
Tomasz <tomash87@users.noreply.github.com>
2019-09-27 14:22:18 +0200

    updated data make script to produce additional validation set and use hit and run in all problems except zqueens1 and zsteinerbaum for which the feasible regions are small enough to enumerate all solutions

--- datasets/ZIMPL/make.py
+++ datasets/ZIMPL/make.py
@@ -17,8 +17,8 @@ def main():
                 generate(interpreter, name_noext + "_training.csv.bz2", 5000, True)
                 print("Calculating test set...")
                 generate(interpreter, name_noext + "_test.csv.bz2", 5000, True)
-                # print("Calculating validation set...")
-                # generate(interpreter, name_noext + "_validation.csv.bz2", 500000, False)
+                print("Calculating validation set...")
+                generate(interpreter, name_noext + "_validation.csv.bz2", 5000, True)
                 # print("Calculating validation set 2...")
                 # generate(interpreter, name_noext + "_validation2.csv.bz2", 500000, False)
                 print("Generating grammar...")
@@ -28,8 +28,11 @@ def main():
 
 def generate(interpreter, filename, n, positive_only=True, class_column=False):
     if not os.path.exists(filename):
-        if positive_only and not class_column:  # and ("queens" in filename or "tsp" in filename or "facility" in filename):
-            data = interpreter.sample_positive(n)
+        if positive_only and not class_column:
+            if "zsteinerbaum" in filename or "zqueens1" in filename:
+                data = interpreter.sample_positive(n, method="bc")
+            else:
+                data = interpreter.sample_positive(n, method="har", budget=1000000)
         else:
             data = interpreter.sample(n, positive_only, class_column)
         data.to_csv(filename, index=False, compression="bz2")
